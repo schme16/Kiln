b924ec54ca8dc8cca266eb03ce16b0ff9cbae8ec
Shane Gadsby <schme16@gmail.com>
2024-04-09 14:07:33 +1000

    cleanup

--- public/sys/js/main.js
+++ public/sys/js/main.js
@@ -1,43 +1,21 @@
-angular.module('Sample ILAR', [])
+angular.module('Kiln Manager', [])
 
-/*The master controller*/
-.controller('master', ($scope, $sce) => {
-	m = $scope
+	/*The master controller*/
+	.controller('master', ($scope, $sce) => {
+		m = $scope
 
-	//Trust a string as rendereable HTML
-	m.trustAsHtml = $sce.trustAsHtml
+		//Trust a string as rendereable HTML
+		m.trustAsHtml = $sce.trustAsHtml
 
-	//App name
-	m.app = 'Sample ILAR'
-
-	m.initialize = () => {
-		//Put the stuff you want to be run when initializing the client side app here
-
-		reportAnalytics('Sample ILAR', 'Sample', 'loaded', {
-			user: m.user,
-			activity: m.activity
-		})
-	}
-
-	//fetch the session data
-	fetchActivity((err, data) => {
-		if (!err) {
-			m.user = data.user
-			m.activity = data.activity
-			m.initialize()
-		}
-		else {
-			console.log(err)
-		}
 	})
-})
 
-/*Turns off the ng-scope, et al. debug classes*/
-.config([
-	'$compileProvider', ($compileProvider) => {
-		$compileProvider.debugInfoEnabled(false)
-	}
-])
+	/*Turns off the ng-scope, et al. debug classes*/
+	.config([
+		'$compileProvider',
+		($compileProvider) => {
+			$compileProvider.debugInfoEnabled(false)
+		}
+	])
 
 
 /*Sample directive*/
